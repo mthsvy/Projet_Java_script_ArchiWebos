c57a625e7a203538554504cb51c141221f5b0b94
Mathias <mthsvilly@gmail.com>
2023-07-05 10:47:02 +0200

    Affichage des categories a partir de l'API

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/Javascript/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/Javascript/script.js
@@ -19,7 +19,7 @@ async function fetchData() {
     const figure = document.createElement("figure");
     const figcaption = document.createElement("figcaption");
 
-    //AJOUT DES TITEZ SOUS CHAQUES IMAGE A PARTIR DE L'API A L'HTML
+    //AJOUT DES TITRE SOUS CHAQUES IMAGE A PARTIR DE L'API A L'HTML
     figcaption.innerText = projets[i].title;
 
     //AJOUT DES BALISE IMG ET FIGCAPTION DANS LA BALISE FIGURE
@@ -33,32 +33,34 @@ async function fetchData() {
 
 fetchData();
 
-//CREATION BALISE BUTTON
-const btn_tous = document.createElement("button");
-const btn_objets = document.createElement("button");
-const btn_appartements = document.createElement("button");
-const btn_hotel_restaurant = document.createElement("button");
-
-//AJOUT DES CLASSE AU BOUTTON
-btn_tous.classList.add("button_color");
-btn_objets.classList.add("button_color");
-btn_appartements.classList.add("button_color");
-btn_hotel_restaurant.classList.add("button_color");
-
-//AJOUT DU TEXTE AU BOUTON
-btn_tous.innerText = "Tous";
-btn_objets.innerText = "Objets";
-btn_appartements.innerText = "Appartements";
-btn_hotel_restaurant.innerText = "Hôtel & Restaurant";
-
 //CREATION DE LA BALISE PARENT DIV BUTTON ALIGN
 const button_align = document.createElement("div");
-
-//AJOUT DES BOUTON DANS LE PARENT
+//CREATION DU BOUTON TOUS
+const btn_tous = document.createElement("button");
+btn_tous.classList.add("button_color");
+btn_tous.innerText = "Tous";
 button_align.appendChild(btn_tous);
-button_align.appendChild(btn_objets);
-button_align.appendChild(btn_appartements);
-button_align.appendChild(btn_hotel_restaurant);
+btn_tous.addEventListener("click", function () {
+  document.querySelector(".gallery").innerHTML = "";
+  fetchData();
+});
+
+//AFFICHAGE DES BOUTONS DES CATEGORIES A PARTIR DE LAPI
+async function getCategorie() {
+  const response = await fetch("http://localhost:5678/api/categories");
+  const categorie = await response.json();
+  for (let i = 0; i < categorie.length; i++) {
+    const btn = document.createElement("button");
+    btn.classList.add("button_color");
+    btn.innerText = categorie[i].name;
+    button_align.appendChild(btn);
+    btn.addEventListener("click", function () {
+      afficherProjetsParCategorie(categorie[i].name);
+    });
+  }
+}
+
+getCategorie();
 
 //AJOUT DE LA CLASSE AU PARENT
 button_align.classList.add("button_align");
@@ -66,16 +68,8 @@ button_align.classList.add("button_align");
 //SELECTIONNER LELEMENT OU AJOUTER LE PARENT
 const position = document.querySelector("#portfolio h2");
 
-//ON AJOUTE LE PARENT DANS LE HMTL VIA A LA POSITION DEFINI JUSTE AVANT
+//ON AJOUTE LE PARENT DANS LE HMTL VIA LA POSITION DEFINI JUSTE AVANT
 position.insertAdjacentElement("afterend", button_align);
-
-//CREATION DES FILTRE
-
-//AJOUT DUN EVENLISTENER
-btn_tous.addEventListener("click", function () {
-  document.querySelector(".gallery").innerHTML = "";
-  fetchData();
-});
 
 //FACTORISER
 async function afficherProjetsParCategorie(categories) {
@@ -113,20 +107,3 @@ async function afficherProjetsParCategorie(categories) {
     gallery.appendChild(figure);
   }
 }
-
-// AJOUT D'UN EVENT LISTENER POUR LES OBJETS
-btn_objets.addEventListener("click", function () {
-  afficherProjetsParCategorie("Objets");
-});
-
-// AJOUT D'UN EVENT LISTENER POUR LES APPARTEMENT
-btn_appartements.addEventListener("click", function () {
-  afficherProjetsParCategorie("Appartements");
-});
-
-// AJOUT D'UN EVENT LISTENER POUR LES HOTEL & RESTAURANTS
-btn_hotel_restaurant.addEventListener("click", function () {
-  afficherProjetsParCategorie("Hotels & restaurants");
-});
-
-
