e90ec77025b70b25217660171a2e2c96eff5b88c
Mathias <mthsvilly@gmail.com>
2023-07-06 11:27:22 +0200

    Bandeau et modifier en mode connecter

--- Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/Javascript/script.js
+++ Portfolio-architecte-sophie-bluel-master/FrontEnd/assets/Javascript/script.js
@@ -35,6 +35,7 @@ fetchData();
 
 //CREATION DE LA BALISE PARENT DIV BUTTON ALIGN
 const button_align = document.createElement("div");
+
 //CREATION DU BOUTON TOUS
 const btn_tous = document.createElement("button");
 btn_tous.classList.add("button_color");
@@ -66,7 +67,7 @@ getCategorie();
 button_align.classList.add("button_align");
 
 //SELECTIONNER LELEMENT OU AJOUTER LE PARENT
-const position = document.querySelector("#portfolio h2");
+const position = document.querySelector(".align_modify");
 
 //ON AJOUTE LE PARENT DANS LE HMTL VIA LA POSITION DEFINI JUSTE AVANT
 position.insertAdjacentElement("afterend", button_align);
@@ -106,4 +107,45 @@ async function afficherProjetsParCategorie(categories) {
     //AJOUT DES BALISES FIGURE AU PARENT
     gallery.appendChild(figure);
   }
+}
+
+//--------------------------------------------------LOGIN LOGOUT---------------------------------
+//TRANSFORMER LE LOGIN EN LOGOUT
+
+//SI ON EST CONNECTER
+if (localStorage.getItem("token")) {
+  //CREE UN LELEMENT
+  const logout = document.createElement("li");
+  logout.innerText = "logout";
+  logout.id = "idLogout";
+  logout.style.cursor = "pointer";
+
+  //SELECTIONNER LE PARENT DE LA LISTE
+  const login = document.querySelector("#login");
+
+  //AJOUTER LELEMENT LOGOUT A LHTML
+  login.insertAdjacentElement("afterend", logout);
+
+  //SUPPRIMER LE LOGIN POUR LE REMPLACER PAR LE LOGOUT
+  login.remove();
+
+  //MASQUER LES FILTRES
+  button_align.style.visibility = "hidden";
+
+  //SELECTIONNER LE BANDEAU
+  const headband = document.querySelector(".headband");
+
+  //AFFICHER LE BANDEAU
+  headband.style.display = "flex";
+
+  //-------------------------------------------------SE DECONNECTER------------------------------
+
+  //AJOUTE UN ECOUTEUR
+  logout.addEventListener("click", function () {
+    //REACHARGE LA PAGE POUR RECUPERER LE LI LOGIN
+    location.reload();
+
+    //SUPPRIME LE TOKEN
+    localStorage.removeItem("token");
+  });
 }
